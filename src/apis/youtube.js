af673c0f838d708f12dfff72312c7e9fb5f16d9c
murat <muratorhun@yahoo.com>
2022-02-01 23:19:00 +0300

    ready for deploy

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import KEY from './keys';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    type: 'video',
+    maxResults: 5,
+    key: KEY,
+  },
+});
---
+++ src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState('');
+
+  const onSubmit = event => {
+    event.preventDefault();
+
+    onFormSubmit(term);
+  };
+
+  return (
+    <div className="search-bar ui segment">
+      <form className="ui form" onSubmit={onSubmit}>
+        <div className="field">
+          <label></label>
+          <input
+            type="text"
+            placeholder="Search"
+            value={term}
+            onChange={event => setTerm(event.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
